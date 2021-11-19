3167a1eb3d946c2f8f659eb8145bc4672c56623f
shynu.v <80030442+shynu-v@users.noreply.github.com>
2021-11-19 09:53:41 +0530

    add

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var response =[];
-var products =[];
+let response =[];
+let products =[];
+var list ="";
 
 window.addEventListener("load",load);
 
@@ -8,7 +9,7 @@ function load(){
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             response = JSON.parse(this.responseText);
-            products = response.products;
+            products = response.products; 
             getdata();
         }
     }
@@ -17,7 +18,7 @@ function load(){
 }
 
 function getdata(){
-    var list =""
+    
     for(i=0;i<products.length;i++){
         list +=`<tr>
                 <td>${products[i].SN}</td>
@@ -27,6 +28,7 @@ function getdata(){
                 <td>${products[i].Department}</td>
                 <td>${products[i].Notes}</td>
                 <td><input type="checkbox" onchange="strike(this)"></td>
+                <td><button class="tdbtn" onclick="rowdel(this)"><i class="far fa-trash-alt"></i></button></td>
                 </tr>`
     }
     document.getElementById("data").innerHTML=list;
@@ -35,8 +37,53 @@ function getdata(){
 function strike(othis) {
     othis.parentNode.parentNode.style.textDecoration = othis.checked? 'line-through':'none';
     othis.parentNode.parentNode.style.color = othis.checked? '#B40000':'black';
+   
 }
 
-function adddata(){
+function rowdel(a)
+{
+  var i = a.parentNode.parentNode.rowIndex;
+  document.getElementById("mytable").deleteRow(i)
+}
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+function submit(){
+    var SN = response.products.length + 1;
+    var Name = document.getElementById("Name").value;
+    var Quantity= document.getElementById("Quantity").value;
+    var unit = document.getElementById("Unit").value;
+    var Department = document.getElementById("Department").value;
+    var Notes = document.getElementById("Notes").value;
+
+    response["products"].push({
+      "SN":SN,
+      "Name":Name,
+      "Quantity":Quantity,
+      "Unit":unit,
+      "Department":Department,
+      "Notes":Notes
+    });
     
+    products = response.products;
+    getdata();
 }
